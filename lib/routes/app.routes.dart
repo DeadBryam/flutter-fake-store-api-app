960032287d98d0714206a225b21c2a483291e161
DeadBryam <Bryamu.u@gmail.com>
2024-04-13 23:45:26 -0600

    refactor: add routes

--- lib/routes/app.routes.dart
+++ lib/routes/app.routes.dart
@@ -1,4 +1,5 @@
 import 'package:cart/features/features.dart';
+import 'package:cart/routes/app.middleware.dart';
 import 'package:get/get.dart';
 
 class Routes {
@@ -9,12 +10,30 @@ class Routes {
   static const String LOGIN = '/login';
   static const String CART = '/cart';
   static const String PRODUCT = '/product';
+  static const String PROFILE = '/profile';
+  static const String PRODUCT_DETAIL = '/product-detail/:id';
 
   static List<GetPage> routes = [
     GetPage(
       name: SPLASH,
       page: SplashPresentation.new,
       binding: SplashBinding(),
+    ),
+    GetPage(
+      name: HOME,
+      page: HomePresentation.new,
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: LOGIN,
+      page: LoginPresentation.new,
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: PROFILE,
+      page: ProfilePresentation.new,
+      binding: ProfileBinding(),
+      middlewares: [AuthMiddleware()],
     ),
   ];
 }
