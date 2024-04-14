056f653923e07f4d133a858c488216b3026ca235
DeadBryam <Bryamu.u@gmail.com>
2024-04-14 10:49:27 -0600

    refactor(product): use cart service

--- lib/features/product/controllers/product.controller.dart
+++ lib/features/product/controllers/product.controller.dart
@@ -9,19 +9,17 @@ class ProductController extends GetxController {
   final _id = RxnInt();
   final _product = Rxn<Product>();
   final _isFetching = true.obs;
-  final _isAddingToCart = false.obs;
-  final _isAddedToCart = false.obs;
   final _quantity = 1.obs;
 
   final _api = Get.find<ApiService>();
-  final _auth = Get.find<AuthService>();
+  final _cart = Get.find<CartService>();
 
   int? get id => _id.value;
   Product? get product => _product.value;
   bool get isFetching => _isFetching.value;
   int get quantity => _quantity.value;
-  bool get isAddingToCart => _isAddingToCart.value;
-  bool get isAddedToCart => _isAddedToCart.value;
+  bool get isAddingToCart => _cart.isFetching;
+  bool get isAddedToCart => _cart.isProductInCart(product?.id ?? 0);
 
   Future<void> fetchProduct() async {
     try {
@@ -44,35 +42,16 @@ class ProductController extends GetxController {
 
   Future<void> addToCart() async {
     try {
-      _isAddingToCart.value = true;
-      final res = await _api.addToCart(
-        userId: _auth.id!,
-        products: [
-          {
-            'productId': product?.id,
-            'quantity': quantity,
-          },
-        ],
+      await _cart.addToCart(
+        product: _product.value,
+        quantity: _quantity.value,
       );
-
-      if (res.isOk) {
-        _isAddedToCart.value = true;
-        Get.snackbar(
-          'success'.tr,
-          'product_added_to_cart'.tr,
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.green,
-        );
-      }
     } catch (e) {
       Get.snackbar(
         'error'.tr,
         'failed_to_add_product_to_cart'.tr,
-        snackPosition: SnackPosition.BOTTOM,
         backgroundColor: Colors.red,
       );
-    } finally {
-      _isAddingToCart.value = false;
     }
   }
 
