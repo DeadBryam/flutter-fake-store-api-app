63f073c065495e655d35afc1b7b3d28ba1f4a464
DeadBryam <Bryamu.u@gmail.com>
2024-04-14 02:09:32 -0600

    refactor(api): add cart api calls

--- lib/services/api.service.dart
+++ lib/services/api.service.dart
@@ -47,4 +47,29 @@ class ApiService extends GetxService {
       'users/$id',
     );
   }
+
+  Future<Response> addToCart({
+    required int userId,
+    required List<Map<String, dynamic>> products,
+  }) {
+    final date = DateTime.now();
+    final body = {
+      'userId': userId,
+      'date': date.toIso8601String(),
+      'products': products,
+    };
+
+    return _apiClient.execute(
+      RequestType.POST,
+      'carts',
+      body: body,
+    );
+  }
+
+  Future<Response> getCartByUserId(int userId) {
+    return _apiClient.execute(
+      RequestType.GET,
+      'carts/user/$userId',
+    );
+  }
 }
