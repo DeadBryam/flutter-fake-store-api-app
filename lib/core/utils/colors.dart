e027b6b55fde8487b148b8971f112ea253ef7b6c
DeadBryam <Bryamu.u@gmail.com>
2024-04-14 00:05:05 -0600

    refactor: update colors

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class ColorsUtil {
   static const Color primaryColor = Color(0xFF8B322C);
   static const Color secondaryColor = Color(0xFFDD5746);
-  static const Color tertiaryColor = Color(0xFFDD5746);
+  static const Color tertiaryColor = Color(0xFFFFC470);
   static const Color quaternaryColor = Color(0xFF4793AF);
 }
