725ee72e5697624cbbe76ddb56ce222a8d8120c8
DeadBryam <Bryamu.u@gmail.com>
2024-04-13 20:55:29 -0600

    feat: add product dto

---
+++ lib/data/dto/product.dart
@@ -0,0 +1,21 @@
+import 'package:cart/data/dto/dto.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product.freezed.dart';
+part 'product.g.dart';
+
+@freezed
+class Product with _$Product {
+  factory Product({
+    int? id,
+    String? title,
+    double? price,
+    String? description,
+    String? category,
+    String? image,
+    Rating? rating,
+  }) = _Product;
+
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
+}
