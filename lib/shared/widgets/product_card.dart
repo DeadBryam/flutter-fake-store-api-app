1f3432bebb1eee4e9ef76666e774e7b791a50d6c
DeadBryam <Bryamu.u@gmail.com>
2024-04-14 01:10:04 -0600

    refactor(widgets): add product card tappeable

--- lib/shared/widgets/product_card.dart
+++ lib/shared/widgets/product_card.dart
@@ -11,58 +11,66 @@ class ProductCard extends StatelessWidget {
   final Product product;
   final void Function(Product)? onTap;
 
-  //  int? id,
-  // String? title,
-  // double? price,
-  // String? description,
-  // String? category,
-  // String? image,
-  // Rating? rating,
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      surfaceTintColor: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              width: double.maxFinite,
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Image.network(
-                product.image ?? '',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.title ?? '',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
+    return Stack(
+      children: [
+        Card(
+          margin: EdgeInsets.zero,
+          surfaceTintColor: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.maxFinite,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Image.network(
+                    product.image ?? '',
                   ),
                 ),
-                const SizedBox(height: 5),
-                Text(
-                  '\$${product.price}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title ?? '',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      '\$${product.price}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
+            ],
+          ),
+        ),
+        Positioned.fill(
+          child: Material(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              hoverColor: Colors.red,
+              onTap: () => onTap?.call(product),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
